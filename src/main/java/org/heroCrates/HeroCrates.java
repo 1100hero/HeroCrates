42d36bde2f064ec39918eadd9520b5636d8fe31b
1100hero <contoriky@gmail.com>
2024-12-01 17:39:25 +0100

    Initial commit

---
+++ src/main/java/org/heroCrates/HeroCrates.java
@@ -0,0 +1,17 @@
+package org.heroCrates;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HeroCrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
